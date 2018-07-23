6c8c61b28bfb51974d00276644ffc125fe62d416
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-07-23 12:30:11 -0700

    Possible fix to build errors

--- src/main/frontend/js/helper.js
+++ src/main/frontend/js/helper.js
@@ -90,7 +90,7 @@ function toggleThis(e) {
     }
 }
 
-LocalStorageManager = class LocalStorageManager{
+var LocalStorageManager = class LocalStorageManager{
     constructor(name) {
         this.name = name;
         this.vls = {};
