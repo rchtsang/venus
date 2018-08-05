a6cdf26229657bd36a9b87133cd1cddce28b5465
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-04 18:51:41 -0700

    Possible fix for the tests timing out.

--- src/test/kotlin/simulator/Lab3Test.kt
+++ src/test/kotlin/simulator/Lab3Test.kt
@@ -24,7 +24,7 @@ fib: 		beq     t3, x0, finish
 		mv      t0, t1
 		mv      t1, t2
 		addi    t3, t3, -1
-		venus.riscv.insts.integer.base.getJal     x0, fib
+		jal     x0, fib
 finish: addi    a0, x0, 1
 		addi    a1, t0, 0
 		# ecall
