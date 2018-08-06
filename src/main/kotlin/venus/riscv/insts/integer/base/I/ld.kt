ab94a910464a79f01e8aeddfd925d45dbca91c9c
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-06 12:12:34 -0700

    Fixed styling

--- src/main/kotlin/venus/riscv/insts/integer/base/I/ld.kt
+++ src/main/kotlin/venus/riscv/insts/integer/base/I/ld.kt
@@ -8,7 +8,7 @@ val ld = LoadTypeInstruction(
         opcode = 0b0000011,
         funct3 = 0b011,
 //        load16 = Simulator::loadWordwCache
-        load32 = {a, b ->
+        load32 = { a, b ->
             throw InstructionNotSupportedError("LD is not supported by 32 bit systems!")
         }
 //        load64 = Simulator::loadWordwCache
--- src/main/kotlin/venus/riscv/insts/integer/base/S/sd.kt
+++ src/main/kotlin/venus/riscv/insts/integer/base/S/sd.kt
@@ -8,7 +8,7 @@ val sd = STypeInstruction(
         opcode = 0b0100011,
         funct3 = 0b011,
 //        store16 = Simulator::storeWordwCache,
-        store32 = {sim, a, b ->
+        store32 = { sim, a, b ->
             throw InstructionNotSupportedError("SD is not supported by 32 bit systems!")
         }
 //        store64 = Simulator::storeWordwCache,
