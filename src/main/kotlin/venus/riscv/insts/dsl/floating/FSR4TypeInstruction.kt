912ef39a6249ab97317591b78208e515bdf5b2e6
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-06 16:31:33 -0700

    Added fnm/fm insts

---
+++ src/main/kotlin/venus/riscv/insts/dsl/floating/FSR4TypeInstruction.kt
@@ -0,0 +1,26 @@
+package venus.riscv.insts.dsl.floating
+
+import venus.riscv.insts.dsl.Instruction
+import venus.riscv.insts.dsl.disasms.extensions.FR4TypeDisassembler
+import venus.riscv.insts.dsl.formats.extensions.R4TypeFormat
+import venus.riscv.insts.dsl.impls.NoImplementation
+import venus.riscv.insts.dsl.impls.extensions.FR4TypeImplementation32
+import venus.riscv.insts.dsl.parsers.extensions.FR4TypeParser
+class FSR4TypeInstruction(
+        name: String,
+        opcode: Int,
+        funct2: Int,
+        //eval16: (Short, Short) -> Short = { _, _ -> throw NotImplementedError("no rv16") },
+        eval32: (Float, Float, Float) -> Float//,
+        //eval64: (Long, Long) -> Long = { _, _ -> throw NotImplementedError("no rv64") },
+        //eval128: (Long, Long) -> Long = { _, _ -> throw NotImplementedError("no rv128") }
+) : Instruction(
+        name = name,
+        format = R4TypeFormat(opcode, funct2),
+        parser = FR4TypeParser,
+        impl16 = NoImplementation,
+        impl32 = FR4TypeImplementation32(eval32),
+        impl64 = NoImplementation,
+        impl128 = NoImplementation,
+        disasm = FR4TypeDisassembler
+)
--- src/main/kotlin/venus/riscv/insts/floating/single/R/fsqrt.s.kt
+++ src/main/kotlin/venus/riscv/insts/floating/single/R/fsqrt.s.kt
@@ -1,3 +1,13 @@
 package venus.riscv.insts.floating.single.r
 
+import venus.riscv.insts.dsl.floating.FSRRTypeInstruction
+import kotlin.js.Math
+
 /*Single-Precision*/
+val fsqrts = FSRRTypeInstruction(
+        name = "fsqrt.s",
+        opcode = 0b1010011,
+        funct7 = 0b0101100,
+        rs2 = 0b00000,
+        eval32 = { a, b -> Math.sqrt(a.toDouble()).toFloat() }
+)
