2f5242f27489e9ca173fdb9698f81573776c944c
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-06 16:42:38 -0700

    Added basic floating comparison

---
+++ src/main/kotlin/venus/riscv/insts/dsl/floating/FFRRTypeInstruction.kt
@@ -0,0 +1,28 @@
+package venus.riscv.insts.dsl.floating
+
+import venus.riscv.insts.dsl.Instruction
+import venus.riscv.insts.dsl.disasms.extensions.FFRRTypeDisassembler
+import venus.riscv.insts.dsl.formats.base.RTypeFormat
+import venus.riscv.insts.dsl.impls.NoImplementation
+import venus.riscv.insts.dsl.impls.extensions.FFRRTypeImplementation32
+import venus.riscv.insts.dsl.parsers.extensions.FFRRTypeParser
+
+class FFRRTypeInstruction(
+        name: String,
+        opcode: Int,
+        funct3: Int,
+        funct7: Int,
+        //eval16: (Short, Short) -> Short = { _, _ -> throw NotImplementedError("no rv16") },
+        eval32: (Float, Float) -> Int//,
+        //eval64: (Long, Long) -> Long = { _, _ -> throw NotImplementedError("no rv64") },
+        //eval128: (Long, Long) -> Long = { _, _ -> throw NotImplementedError("no rv128") }
+) : Instruction(
+        name = name,
+        format = RTypeFormat(opcode, funct3, funct7),
+        parser = FFRRTypeParser,
+        impl16 = NoImplementation,
+        impl32 = FFRRTypeImplementation32(eval32),
+        impl64 = NoImplementation,
+        impl128 = NoImplementation,
+        disasm = FFRRTypeDisassembler
+)
--- src/main/kotlin/venus/riscv/insts/dsl/impls/extensions/FFRRTypeImplementation32.kt
+++ src/main/kotlin/venus/riscv/insts/dsl/impls/extensions/FFRRTypeImplementation32.kt
@@ -1,5 +1,6 @@
 package venus.riscv.insts.dsl.impls.extensions
 
+import venus.riscv.InstructionField
 import venus.riscv.MachineCode
 import venus.riscv.insts.dsl.impls.InstructionImplementation
 import venus.simulator.Simulator
@@ -7,8 +8,14 @@ import venus.simulator.Simulator
 /**
  * Created by thaum on 8/6/2018.
  */
-class FFRRTypeImplementation32(private val eval: (Int, Int) -> Int) : InstructionImplementation {
+class FFRRTypeImplementation32(private val eval: (Float, Float) -> Int) : InstructionImplementation {
     override operator fun invoke(mcode: MachineCode, sim: Simulator) {
-        throw NotImplementedError("Working on floating impls.")
+        val rs1 = mcode[InstructionField.RS1]
+        val rs2 = mcode[InstructionField.RS2]
+        val rd = mcode[InstructionField.RD]
+        val vrs1 = sim.getFReg(rs1)
+        val vrs2 = sim.getFReg(rs2)
+        sim.setReg(rd, eval(vrs1, vrs2))
+        sim.incrementPC(mcode.length)
     }
 }
