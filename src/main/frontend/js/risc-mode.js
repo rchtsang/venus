a5f0be3a3a9a278bb5e63bf1eeaae53b1b357860
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-06 13:06:05 -0700

    Added check to register decode to confirm that the register is an integer register for integer ops and floating for floating ops.

--- src/main/frontend/js/risc-mode.js
+++ src/main/frontend/js/risc-mode.js
@@ -145,7 +145,7 @@ CodeMirror.defineMode("riscv", function(config, parserConfig) {
     var registers = regexFromWords([
         "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15",
         "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31",
-        "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+        "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "fp", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
         "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
         "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",
         "f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23", "f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31",
@@ -165,12 +165,12 @@ CodeMirror.defineMode("riscv", function(config, parserConfig) {
             stream.skipToEnd();
             return "comment";
         }
-        
+
         if (ch == "\"" || ch == "'") {
             state.cur = string(ch)
             return state.cur(stream, state);
         }
-        
+
         if (/\d/.test(ch)) {
             stream.eatWhile(/[\w.%]/);
             return "number";
@@ -180,6 +180,7 @@ CodeMirror.defineMode("riscv", function(config, parserConfig) {
             stream.eatWhile(/[\w\\\-_.]/);
             return "variable";
         }
+
 
         return null;
     }
