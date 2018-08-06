a706b780e5e41f03ebc2d41bf48e728f058ff18c
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-08-06 12:03:52 -0700

    Fixed sd

--- src/main/kotlin/venus/riscv/insts/integer/base/S/sd.kt
+++ src/main/kotlin/venus/riscv/insts/integer/base/S/sd.kt
@@ -4,9 +4,9 @@ import venus.riscv.insts.InstructionNotSupportedError
 import venus.riscv.insts.dsl.STypeInstruction
 
 val sd = STypeInstruction(
-        name = "sw",
+        name = "sd",
         opcode = 0b0100011,
-        funct3 = 0b010,
+        funct3 = 0b011,
 //        store16 = Simulator::storeWordwCache,
         store32 = {sim, a, b ->
             throw InstructionNotSupportedError("SD is not supported by 32 bit systems!")
