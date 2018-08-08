a4605dc6ad5e98c7415cc610036202bafa21addb
ThaumicMekanism <ThaumicMekanism@gmail.com>
2018-08-07 23:24:52 -0700

    Fixed store formatting issue

--- src/main/kotlin/venus/riscv/insts/floating/single/s/fsw.kt
+++ src/main/kotlin/venus/riscv/insts/floating/single/s/fsw.kt
@@ -1,16 +1,16 @@
 package venus.riscv.insts.floating.single.s
 
-import venus.riscv.insts.dsl.STypeInstruction
+import venus.riscv.insts.dsl.floating.FSTypeInstruction
 
 /*Single-Precision*/
 
-val fsw = STypeInstruction(
+val fsw = FSTypeInstruction(
         name = "fsw",
         opcode = 0b0100111,
         funct3 = 0b010,
 //        store16 = NoImplementation,
         store32 = { sim, address, value ->
-            sim.storeWordwCache(address, value.toFloat().toRawBits())
+            sim.storeWordwCache(address, value.getFloat().toRawBits())
         }
 //        store64 = NoImplementation,
 //        store128 = NoImplementation
