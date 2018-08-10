958de6e4bd7ffa141483b7415dc7841a776a982a
ThaumicMekanism <ThaumicMekanism@gmail.com>
2018-08-09 17:44:00 -0700

    Added basic impls to other insts

--- src/main/kotlin/venus/riscv/insts/integer/base/r/addw.kt
+++ src/main/kotlin/venus/riscv/insts/integer/base/r/addw.kt
@@ -1,1 +1,14 @@
 package venus.riscv.insts.integer.base.r
+
+import venus.riscv.insts.InstructionNotSupportedError
+import venus.riscv.insts.dsl.RTypeInstruction
+
+val addw = RTypeInstruction(
+        name = "addw",
+        opcode = 0b0111011,
+        funct3 = 0b000,
+        funct7 = 0b0000000,
+        eval32 = { a, b ->
+            throw InstructionNotSupportedError("addiw is not supported on 32 bit systems!")
+        }
+)
--- src/main/kotlin/venus/riscv/insts/integer/base/r/sraw.kt
+++ src/main/kotlin/venus/riscv/insts/integer/base/r/sraw.kt
@@ -1,1 +1,14 @@
 package venus.riscv.insts.integer.base.r
+
+import venus.riscv.insts.InstructionNotSupportedError
+import venus.riscv.insts.dsl.RTypeInstruction
+
+val sraw = RTypeInstruction(
+        name = "sraw",
+        opcode = 0b0111011,
+        funct3 = 0b101,
+        funct7 = 0b0100000,
+        eval32 = { a, b ->
+            throw InstructionNotSupportedError("addiw is not supported on 32 bit systems!")
+        }
+)
