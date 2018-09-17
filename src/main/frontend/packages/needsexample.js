8dfc8576b0dc81408299e6564338cf8d297f8ea8
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-09-17 11:33:57 -0700

    Reworked packages a little

--- src/main/frontend/packages/needsexample.js
+++ src/main/frontend/packages/needsexample.js
@@ -2,10 +2,10 @@
 var venuspackage = {
     id: "requirespackage",
     requires: ["examplepackage"],
-    load: function() {
+    load: function(setting) {
         console.warn("I only work with examplepackage!.");
     },
-    unload: function() {
+    unload: function(setting) {
         console.warn("I have unloaded the package! This is the stage which all of the changes and hooks should be undone.")
     }
 };
