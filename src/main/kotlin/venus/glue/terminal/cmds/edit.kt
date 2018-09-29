e57660fba1249225020b06c1682575701573dca3
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-09-28 23:21:27 -0700

    Added save and edit feature

---
+++ src/main/kotlin/venus/glue/terminal/cmds/edit.kt
@@ -0,0 +1,32 @@
+package venus.glue.terminal.cmds
+
+import venus.glue.Driver
+import venus.glue.terminal.Command
+import venus.glue.terminal.Terminal
+import venus.glue.vfs.VFSFile
+import venus.glue.vfs.VFSType
+
+var edit = Command(
+        name = "edit",
+        execute = fun (args: MutableList<String>, t: Terminal, sudo: Boolean): String {
+            if (args.size != 1) {
+                return "edit: Takes in one argument [filename]"
+            }
+            val obj = t.vfs.getObjectFromPath(args[0])
+            if (obj === null) {
+                return "edit: '${args[0]}' could not be found!"
+            }
+            if (obj.type !== VFSType.File) {
+                return "edit: Only files can be loaded into the editor."
+            }
+            try {
+                val txt: String = (obj as VFSFile).readText()
+                js("codeMirror.setValue(txt);")
+                Driver.openEditor()
+                js("codeMirror.refresh();")
+            } catch (e: Throwable) {
+                return "edit: Could not load file to the editor!"
+            }
+            return ""
+        }
+)
