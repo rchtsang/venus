835db52972e34cc95cd5f259562bb30b88e69a2a
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-10-01 09:50:50 -0700

    Fixed bug where mkdir would make a folder which should not exist

--- src/main/kotlin/venus/glue/terminal/cmds/mkdir.kt
+++ src/main/kotlin/venus/glue/terminal/cmds/mkdir.kt
@@ -6,6 +6,9 @@ import venus.glue.terminal.Terminal
 var mkdir = Command(
         name = "mkdir",
         execute = fun (args: MutableList<String>, t: Terminal, sudo: Boolean): String {
+            if (args.size < 1) {
+                return "mkdir: mkdir takes in a folder name."
+            }
             return t.vfs.mkdir(args.joinToString(" "))
         }
 )
