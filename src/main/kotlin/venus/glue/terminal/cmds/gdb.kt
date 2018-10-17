a1b20bfc9a1d6279a29d1d2c609995b6da26f321
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-10-17 12:01:57 -0700

    Added gdb and running abstractions. Fixed linker bug with relocation offsets

--- src/main/kotlin/venus/glue/terminal/cmds/gdb.kt
+++ src/main/kotlin/venus/glue/terminal/cmds/gdb.kt
@@ -1,32 +1,97 @@
 package venus.glue.terminal.cmds
 
+/* ktlint-disable no-wildcard-imports */
+import venus.assembler.Assembler
 import venus.glue.Driver
 import venus.glue.Renderer
 import venus.glue.terminal.Command
 import venus.glue.terminal.Terminal
-import venus.glue.vfs.VFSLinkedProgram
-import venus.glue.vfs.VFSType
+import venus.glue.vfs.*
+import venus.linker.LinkedProgram
+import venus.linker.Linker
 import venus.riscv.Address
 import venus.riscv.MemSize
+import venus.riscv.Program
 import venus.simulator.Simulator
+/* ktlint-enable no-wildcard-imports */
 
 var gdb = Command(
         name = "gdb",
         execute = fun (args: MutableList<String>, t: Terminal, sudo: Boolean): String {
-            if (args.size != 1) {
-                return "gdb: This currently just takes in one argument [a linked program]"
+            if (args.size < 1) {
+                return "gdb: Takes in at least one File/Program or a single LinkedProgram"
             }
-            val obj = t.vfs.getObjectFromPath(args[0])
-            if (obj === null) {
-                return "gdb: Could not find file ${args[0]}!"
+            val programout = "debugger"
+            val fs = args
+            val linkedprogs = ArrayList<LinkedProgram>()
+            val progs = ArrayList<Program>()
+            val files = ArrayList<VFSFile>()
+            for (file in fs) {
+                val obj = t.vfs.getObjectFromPath(file)
+                if (obj == null) {
+                    return "gdb: Could not find file $file"
+                } else {
+                    when (obj.type) {
+                        VFSType.File -> {
+                            files.add(obj as VFSFile)
+                        }
+                        VFSType.Program -> {
+                            progs.add((obj as VFSProgram).getProgram())
+                        }
+                        VFSType.LinkedProgram -> {
+                            if (files.size + progs.size + linkedprogs.size > 0) {
+                                return "gdb: You must either have no linked programs or just one linked program!"
+                            }
+                            linkedprogs.add((obj as VFSLinkedProgram).getLinkedProgram())
+                        }
+                        else -> { return "gdb: Unsupported type: ${obj.type}" }
+                    }
+                }
             }
-            if (obj.type !== VFSType.LinkedProgram) {
-                return "gdb: You must input a linked program!"
+            if (files.size + progs.size + linkedprogs.size == 0) {
+                return "gdb: Could not find any of the inputted files!"
             }
 
+            // Assembly stage for any files
+            var msg = ""
+            for (file in files) {
+                val (prog, errors, warnings) = Assembler.assemble(file.contents.get(VFSFile.innerTxt) as String, file.label)
+                if (errors.isNotEmpty()) {
+                    msg += "assemble: Could not assemble file! Here are the errors:"
+                    for (error in errors) {
+                        msg += "\n" + error.toString()
+                    }
+                    return msg
+                }
+                if (warnings.isNotEmpty()) {
+                    msg += "assemble: Assembled file with a few warnings:"
+                    for (warning in warnings) {
+                        msg += "\n" + warning.toString()
+                    }
+                }
+                progs.add(prog)
+            }
+
+            // Linking Stage for any programs
+            if (progs.size > 0) {
+                val linkedProgram: LinkedProgram
+                try {
+                    linkedProgram = Linker.link(progs)
+                } catch (e: Throwable) {
+                    return "gdb: An error occurred when running the linked program: $e"
+                }
+                linkedprogs.add(linkedProgram)
+            }
+
+            // Getting the LinkedProgram which we want to simulate
+            if (linkedprogs.size != 1) {
+                return msg + "gdb: There must only be one linked program!"
+            }
+            val lp = linkedprogs[0]
             try {
-                val sim = Simulator((obj as VFSLinkedProgram).getLinkedProgram(), t.vfs.simSettings)
+                val sim = Simulator(lp, t.vfs.simSettings)
                 Renderer.renderSimulator()
+                Driver.sim = sim
                 Renderer.loadSimulator(sim)
                 Driver.setCacheSettings()
                 Renderer.updateCache(Address(0, MemSize.WORD))
@@ -37,6 +102,10 @@ var gdb = Command(
             return ""
         },
         tab = fun (args: MutableList<String>, t: Terminal, sudo: Boolean): ArrayList<Any> {
-            throw NotImplementedError()
+            if (args.size == 1) {
+                val prefix = args[args.size - 1]
+                return arrayListOf(prefix, t.vfs.filesFromPrefix(prefix))
+            }
+            return arrayListOf("", ArrayList<String>())
         }
 )
