ac76802d13327271c463ec8624f1d645705011f1
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-10-16 23:00:14 -0700

    Prog and Linked Prog will not be saved for now

--- src/main/kotlin/venus/glue/vfs/VFSDrive.kt
+++ src/main/kotlin/venus/glue/vfs/VFSDrive.kt
@@ -12,15 +12,24 @@ class VFSDrive(val n: String, override var parent: VFSObject) : VFSFolder(n, par
             val folder = VFSDrive(jsonContainer.label.removeSuffix(":"), parent)
             for (i in 0 until js("jsonContainer.contents.length")) {
                 val value = js("jsonContainer.contents[i]")
+                var add = true
                 val obj = when (value.type) {
-                    VFSType.Program.toString() -> { VFSProgram.inflate(value, folder) }
+                    VFSType.Program.toString() -> {
+                        add = false
+                        VFSProgram.inflate(value, folder)
+                    }
                     VFSType.Folder.toString() -> { VFSFolder.inflate(value, folder) }
-                    VFSType.LinkedProgram.toString() -> { VFSLinkedProgram.inflate(value, folder) }
+                    VFSType.LinkedProgram.toString() -> {
+                        add = false
+                        VFSLinkedProgram.inflate(value, folder)
+                    }
                     VFSType.File.toString() -> { VFSFile.inflate(value, folder) }
                     VFSType.Drive.toString() -> { VFSDrive.inflate(value, folder) }
                     else -> { VFSDummy() }
                 }
-                folder.addChild(obj)
+                if (add) {
+                    folder.addChild(obj)
+                }
             }
             return folder
         }
