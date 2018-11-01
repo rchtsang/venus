f85a3950f0add89b62c74400d017ed1909a1fae4
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-11-01 06:36:58 +0000

    Added basic line lexer which will be used for args for js version

--- src/main/kotlin/venus/assembler/Lexer.kt
+++ src/main/kotlin/venus/assembler/Lexer.kt
@@ -65,4 +65,49 @@ object Lexer {
 
         return Pair(labels, previousWords)
     }
+    /**
+     * Lex a line into a label (if there) and a list of arguments.
+     *
+     * @param line the line to lex
+     * @return tokens
+     * @see LineTokens
+     */
+    fun lex(line: String): LineTokens {
+        var currentWord = StringBuilder("")
+        val previousWords = ArrayList<String>()
+        var escaped = false
+        var inCharacter = false
+        var inString = false
+
+        for (ch in line) {
+            var wasDelimiter = false
+            when (ch) {
+                '\'' -> inCharacter = !(escaped xor inCharacter) && !inString
+                '"' -> inString = !(escaped xor inString) && !inCharacter
+                ' ' -> wasDelimiter = !inString && !inCharacter
+//                ' ', '\t', '(', ')', ',' -> wasDelimiter = !inString && !inCharacter
+            }
+            escaped = !escaped && ch == '\\'
+
+            if (wasDelimiter) {
+                val l = currentWord.length
+                if (l > 2 && (currentWord[0].toString() in listOf("'", "\"") && currentWord[l - 1] == currentWord[0])) {
+                    val cs = currentWord.subSequence(1, l - 1)
+                    currentWord = StringBuilder(cs)
+                }
+                addNonemptyWord(previousWords, currentWord)
+                currentWord = StringBuilder("")
+            } else if (escaped) {} else {
+                currentWord.append(ch)
+            }
+        }
+        val l = currentWord.length
+        if (l > 2 && (currentWord[0].toString() in listOf("'", "\"") && currentWord[l - 1] == currentWord[0])) {
+            val cs = currentWord.subSequence(1, l - 1)
+            currentWord = StringBuilder(cs)
+        }
+        addNonemptyWord(previousWords, currentWord)
+
+        return previousWords
+    }
 }
