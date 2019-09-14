e7d1a1de794bd43b3d355134720a35499cef0f75
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-13 23:23:33 -0700

    Added js zip lib

---
+++ src/main/kotlin/venus/terminal/cmds/unzip.kt
@@ -0,0 +1,17 @@
+package venus.terminal.cmds
+
+import venus.terminal.Command
+import venus.terminal.Terminal
+
+var ls = Command(
+        name = "ls",
+        execute = fun(args: MutableList<String>, t: Terminal, sudo: Boolean): String {
+            return t.vfs.ls()
+        },
+        tab = fun(args: MutableList<String>, t: Terminal, sudo: Boolean): ArrayList<Any> {
+            throw NotImplementedError()
+        },
+        help = "This command prints out the contents of the current folder." +
+                "\nIt currently does not take in any arguments." +
+                "\nUsage: ls"
+)
---
+++ src/main/kotlin/venus/zip/zip.kt
@@ -0,0 +1,4 @@
+package venus.zip
+
+class zip {
+}
