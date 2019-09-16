ff894b3beba7b76fe2deb200d6f24d062e907c8b
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-15 23:32:59 -0700

    Fixed some interface bugs with the files. Also made the venus tab the dominant tab

--- src/main/kotlin/venus/Driver.kt
+++ src/main/kotlin/venus/Driver.kt
@@ -496,6 +496,9 @@ import kotlin.dom.removeClass
     @JsName("openVenusTab") fun openVenusTab(tabid: String) {
         val tabs = listOf("venus-terminal", "venus-files", "venus-url", "venus-jvm")
         Renderer.renderTab(tabid, tabs)
+        if (tabid == "venus-files") {
+            refreshVFS()
+        }
     }
 
     /**
