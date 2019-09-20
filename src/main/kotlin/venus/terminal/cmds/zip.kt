9d7c6af8ad4b4be0e2cdd0136ef1d92ef75be8a3
Stephan Kaminsky <thaumicmekanism@gmail.com>
2019-09-20 11:26:10 -0700

    Some basic zip work. Not working yet but still some work

--- src/main/kotlin/venus/terminal/cmds/zip.kt
+++ src/main/kotlin/venus/terminal/cmds/zip.kt
@@ -2,16 +2,34 @@ package venus.terminal.cmds
 
 import venus.terminal.Command
 import venus.terminal.Terminal
+import venus.vfs.VFSDummy
+import venus.vfs.VFSFile
+import venus.vfs.VFSType
+import venus.zip.Zip
 
 var zip = Command(
         name = "zip",
         execute = fun(args: MutableList<String>, t: Terminal, sudo: Boolean): String {
-            throw NotImplementedError()
+            if (args.size < 2) {
+                return "zip: Takes at least two arguments, the zip file name and the file you want to add to the zip."
+            }
+            val s = StringBuilder()
+            var output = args.removeAt(0)
+            val z = Zip()
+            for (fname in args) {
+                val f = t.vfs.getObjectFromPath(fname) ?: VFSDummy()
+                if (f.type == VFSType.File) {
+                    z.addFile(fname, (f as VFSFile).readText())
+                } else {
+                    s.append("'$fname' is not a path to a file! For now, this function only accepts files.\n")
+                }
+            }
+
+            z.save(output)
+            return s.toString()
         },
         tab = fun(args: MutableList<String>, t: Terminal, sudo: Boolean): ArrayList<Any> {
             throw NotImplementedError()
         },
-        help = "This command prints out the contents of the current folder." +
-                "\nIt currently does not take in any arguments." +
-                "\nUsage: ls"
+        help = "This command creates a zip file."
 )
--- src/main/kotlin/venus/zip/zip.kt
+++ src/main/kotlin/venus/zip/zip.kt
@@ -6,6 +6,31 @@ class Zip {
         internal_zip = internal_zip.file(name, data, js("""{"binary":true}"""))
     }
 
+    fun save(name: String): String {
+        val z = internal_zip
+        js("""
+            var saveData = (function () {
+            var a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            return function (data, fileName) {
+                var json = JSON.stringify(data),
+                    blob = new Blob([json], {type: "octet/stream"}),
+                    url = window.URL.createObjectURL(blob);
+                a.href = url;
+                a.download = fileName;
+                a.click();
+                window.URL.revokeObjectURL(url);
+            };
+        }());
+
+           z.generateAsync({"type": "blob"}).then(function(data){
+            saveData(data, name);
+           });
+        """)
+        return ""
+    }
+
 //    fun addFolder()
 }
 
