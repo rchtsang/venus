c7ee612bef08ce6d27832c917b026ccb40d17a19
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-03-05 20:23:51 -0800

    Malloc errors are no longer exceptions thrown but instead just extra data printed out

--- src/main/kotlin/venus/Renderer.kt
+++ src/main/kotlin/venus/Renderer.kt
@@ -72,6 +72,14 @@ internal object Renderer {
         System.err.println(thing)
     }
 
+    fun stdout(thing: Any) {
+        printConsole(thing)
+    }
+
+    fun stderr(thing: Any) {
+        displayError(thing)
+    }
+
     /** a map from integers to the corresponding hex digits */
     private val hexMap = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F')
