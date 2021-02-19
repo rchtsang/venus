d7851d1098dff75d463bbb6e2149c5945885371b
Stephan Kaminsky <thaumicmekanism@gmail.com>
2021-02-19 15:46:19 -0800

    Fixed bug in build process which would stack codemirror below addons to cause an error

--- src/main/frontend/js/codemirror/autorefresh.js
+++ src/main/frontend/js/codemirror/autorefresh.js
@@ -7,33 +7,40 @@
   else if (typeof define == "function" && define.amd) // AMD
     define(["../../lib/codemirror"], mod);
   else // Plain browser env
-    mod(CodeMirror)
+      function checkAndRun() {
+        if (typeof CodeMirror == "undefined") {
+          setTimeout(checkAndRun, 1);
+        } else {
+          mod(CodeMirror);
+        }
+      }
+    checkAndRun();
 })(function(CodeMirror) {
   "use strict";
 
   CodeMirror.defineOption("autoRefresh", false, function(cm, val) {
     if (cm.state.autoRefresh) {
       stopListening(cm, cm.state.autoRefresh);
-      cm.state.autoRefresh = null
+      cm.state.autoRefresh = null;
     }
     if (val && cm.display.wrapper.offsetHeight == 0)
-      startListening(cm, cm.state.autoRefresh = {delay: val.delay || 250})
+      startListening(cm, cm.state.autoRefresh = {delay: val.delay || 250});
   });
 
   function startListening(cm, state) {
     function check() {
       if (cm.display.wrapper.offsetHeight) {
-        stopListening(cm, state)
+        stopListening(cm, state);
         if (cm.display.lastWrapHeight != cm.display.wrapper.clientHeight)
-          cm.refresh()
+          cm.refresh();
       } else {
-        state.timeout = setTimeout(check, state.delay)
+        state.timeout = setTimeout(check, state.delay);
       }
     }
     state.timeout = setTimeout(check, state.delay);
     state.hurry = function() {
       clearTimeout(state.timeout);
-      state.timeout = setTimeout(check, 50)
+      state.timeout = setTimeout(check, 50);
     };
     CodeMirror.on(window, "mouseup", state.hurry);
     CodeMirror.on(window, "keyup", state.hurry);
